b87d132512e24a2d5b740f51b87a217651312e11
ikostin <IKostin@uclan.ac.uk>
2024-03-06 18:08:33 +0200

    New logic + Routing + New Logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -42,6 +44,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
+  int _currentIndexForBar = 0;
+  // TODO: Remove handlers and create separated handlers for segmental routing
+
+  void _onListBoxTapped(int index) {
+    setState(() {
+      _currentIndexForBar = index;
+    });
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -59,70 +69,122 @@ class _MyHomePageState extends State<MyHomePage> {
             alignment: Alignment.center,
             margin: const EdgeInsets.symmetric(vertical: 36.0),
             height: 52.0,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              children: const <Widget>[
-                Align(
-                  alignment: Alignment.center,
-                  child: Image(image: AssetImage('assets/Logo.png'), width: 36,),
-                ),
-                SizedBox(width: 20),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text('FOR YOU', style: TextStyle(fontSize: 24, fontFamily: 'Unbounded'),),
-                ),
-                SizedBox(width: 20),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text('Watching Now', style: TextStyle(fontSize: 16, fontFamily: 'Unbounded', color: Colors.white30),),
-                ),
-                SizedBox(width: 20),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text('New', style: TextStyle(fontSize: 16, fontFamily: 'Unbounded', color: Colors.white30),),
-                ),
-                SizedBox(width: 20),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text('TOP-250', style: TextStyle(fontSize: 16, fontFamily: 'Unbounded', color: Colors.white30),),
-                ),
-              ],
-            ),
+            child: () {
+              if (_currentIndex == 0) {
+                return ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  // TODO: Refactor logic and conditional fontSize. Maybe implement special function for this
+                  children:   <Widget>[
+                    const Align(
+                      alignment: Alignment.center,
+                      child: Image(image: AssetImage('assets/Logo.png'), width: 36),
+                    ),
+                    SizedBox(width: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: InkWell(onTap: () {_onListBoxTapped(0);}, child: Text('FOR YOU', style: TextStyle(fontSize: _currentIndexForBar == 0 ? 24 : 16, color: _currentIndexForBar == 0 ? Colors.white : Colors.white30, fontFamily: 'Unbounded')),),
+                    ),
+                    SizedBox(width: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: InkWell(onTap: () {_onListBoxTapped(1);}, child: Text('Watching Now', style: TextStyle(fontSize: _currentIndexForBar == 1 ? 24 : 16, fontFamily: 'Unbounded', color: _currentIndexForBar == 1 ? Colors.white : Colors.white30)),),
+                    ),
+                    SizedBox(width: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: InkWell(onTap: () {_onListBoxTapped(2);}, child: Text('New', style: TextStyle(fontSize: _currentIndexForBar == 2 ? 24 : 16, fontFamily: 'Unbounded', color: _currentIndexForBar == 2 ? Colors.white : Colors.white30)),),
+                    ),
+                    SizedBox(width: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: InkWell(onTap: () {_onListBoxTapped(3);}, child: Text('TOP-250', style: TextStyle(fontSize: _currentIndexForBar == 3 ? 24 : 16, fontFamily: 'Unbounded', color: _currentIndexForBar == 3 ? Colors.white : Colors.white30)),),
+                    ),
+                  ],
+                );
+              } else {
+                // TODO: Implement reusable search bar
+                return Text('SEARCH BAR');
+              }
+              } ()
           ),
           Expanded(
+            // TODO: 'Remove this war crime and restructure logic. Make new functions and implement all this routing using separated conditions either separated routing library'
             child: Center(
-              child: Text(
-                'PAGE #$_currentIndex',
-                style: const TextStyle(fontSize: 24, color: Colors.white, fontFamily: 'Unbounded'),
-              ),
+              child: () {
+                if (_currentIndex == 0) {
+                  if (_currentIndexForBar == 0) {
+                    return const Column(
+                      children: [
+                        Text('Home Page'),
+                        Text('FOR YOU')
+                      ],
+                    );
+                  } else if (_currentIndexForBar == 1) {
+                    return const Column(
+                      children: [
+                        Text('Home Page'),
+                        Text('Watching Now')
+                      ],
+                    );
+                  } else if (_currentIndexForBar == 2) {
+                    return const Column(
+                      children: [
+                        Text('Home Page'),
+                        Text('NEW')
+                      ],
+                    );
+                  } else {
+                    return const Column(
+                      children: [
+                        Text('Home Page'),
+                        Text('TOP-250')
+                      ],
+                    );
+                  }
+                } else if (_currentIndex == 1){
+                  return const Text('Catalogue');
+                } else if (_currentIndex == 2){
+                  return const Text('Search');
+                } else if (_currentIndex == 3){
+                  return const Text('Profile');
+                } else {
+                  throw ErrorDescription('Wrong index for conditional routing');
+                }
+              } ()
             ),
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0,
-        iconSize: 36,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon:  Icon(Icons.home_rounded),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grid_view_rounded, size: 28),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search_rounded, size: 28),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline, size: 28),
-            label: '',
-          ),
-        ],
-        currentIndex: _currentIndex,
-        onTap: _onItemTapped,
+      // TODO: 'Refactor and create my own Navigation Bar'
+      // TODO: 'Use InkWell or GestureHelper for implementing tapping and visualization'
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BottomNavigationBar(
+          elevation: 0,
+          iconSize: 36,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon:  Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.grid_view_rounded, size: 28),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search_rounded, size: 28),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline, size: 28),
+              label: '',
+            ),
+          ],
+          currentIndex: _currentIndex,
+          // TODO: Remove this handler because using my own navigation bar
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
